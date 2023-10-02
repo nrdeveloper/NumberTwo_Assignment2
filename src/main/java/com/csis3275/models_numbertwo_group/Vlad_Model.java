f114dc40fc475f9afe1533fcf504fcb578fa4508
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-10-02 14:22:24 -0700

    feat: Added Vlad Model

---
+++ src/main/java/com/csis3275/models_numbertwo_group/Vlad_Model.java
@@ -0,0 +1,19 @@
+package com.csis3275.models_numbertwo_group;
+
+public class Vlad_Model {
+	private String name;
+	private String description;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
