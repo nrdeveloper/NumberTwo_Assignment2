0b4ddb1a17398b2b32dff7df68a43bd195b6b2dc
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-10-02 14:21:56 -0700

    feat: Added Home Page Model

---
+++ src/main/java/com/csis3275/models_numbertwo_group/Home_Model.java
@@ -0,0 +1,13 @@
+package com.csis3275.models_numbertwo_group;
+
+public class Home_Model {
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
