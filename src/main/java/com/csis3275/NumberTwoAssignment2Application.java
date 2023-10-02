1d55863d700274dfe030e03c9abe66a48c8546d4
Nahuel Robledo <nahuel.erobledo@gmail.com>
2023-10-02 01:53:57 -0700

    Chore: Eclipse project created

---
+++ src/main/java/com/csis3275/NumberTwoAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberTwoAssignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberTwoAssignment2Application.class, args);
+	}
+
+}
