617a347e00a9fb111517111aed64feee7308f33f
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-10-02 20:17:59 -0700

    Feat: Added home model to the home view

--- src/main/java/com/csis3275/controller_numbertwo_group/Controller_NumberTwo_Group.java
+++ src/main/java/com/csis3275/controller_numbertwo_group/Controller_NumberTwo_Group.java
@@ -1,8 +1,11 @@
 package com.csis3275.controller_numbertwo_group;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.csis3275.models_numbertwo_group.Home_Model;
 
 // Home page controller
 // Displays 3 link/buttons to team member's personal pages and a group description
@@ -13,6 +16,7 @@ public class Controller_NumberTwo_Group {
 	
 	@GetMapping("/")
 	public String homePage_NumberTwo(Model model) {
+		model.addAttribute("home_model", new Home_Model());
 		return "home";
 	}
 }
