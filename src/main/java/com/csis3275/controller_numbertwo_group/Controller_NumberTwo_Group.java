94bba39d9d1e4a813f112647dd05e8540904e969
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-10-02 19:56:28 -0700

    feat: Added home page controller, that shows HomePage_NumberTwo.html when root of the app is opened

--- src/main/java/com/csis3275/controller_numbertwo_group/Controller_NumberTwo_Group.java
+++ src/main/java/com/csis3275/controller_numbertwo_group/Controller_NumberTwo_Group.java
@@ -1,5 +1,18 @@
 package com.csis3275.controller_numbertwo_group;
 
-public class Controller_NumberTwo_Group {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+// Home page controller
+// Displays 3 link/buttons to team member's personal pages and a group description
+// Root directory
+
+@Controller
+public class Controller_NumberTwo_Group {
+	
+	@GetMapping("/")
+	public String homePage_NumberTwo(Model model) {
+		return "HomePage_NumberTwo";
+	}
 }
