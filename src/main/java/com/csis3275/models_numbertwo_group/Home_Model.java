f7db2bdd432c466f522a2a59600fc5e108c2dd4f
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-10-02 21:16:55 -0700

    Chore: Added placeholder group description

--- src/main/java/com/csis3275/models_numbertwo_group/Home_Model.java
+++ src/main/java/com/csis3275/models_numbertwo_group/Home_Model.java
@@ -1,7 +1,10 @@
 package com.csis3275.models_numbertwo_group;
 
 public class Home_Model {
-	private String description;
+	private String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+			+ "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+			+ "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+			+ "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 	public String getDescription() {
 		return description;
